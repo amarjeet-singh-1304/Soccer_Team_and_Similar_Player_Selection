bd6084eab3bc52656de2ac3bfdb75554954bc174
amarjeet singh <asing499@asu.edu>
2022-11-21 19:45:51 -0700

    Updating the code files and models

--- wf_ml_evaluation.py
+++ wf_ml_evaluation.py
@@ -2,8 +2,22 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import pickle
+import warnings
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LinearRegression
+import numpy as np
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import mean_squared_error
 import wf_ml_prediction
 import wf_ml_training
+
+
+warnings.filterwarnings("ignore")
+evaluation_folder = "evaluation/"
+evaluation_file_name = "summary.txt"
+model_folder = "models/"
 
 feature_cols = ['potential', 'pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic',
                 'attacking_crossing', 'attacking_finishing',
@@ -24,13 +38,62 @@ feature_cols = ['potential', 'pace', 'shooting', 'passing', 'dribbling', 'defend
 
 
 def experimentation():
-    model_folder = "models/"
     model_file_name = "linear_regression_model.sav"
     reg_model = pickle.load(open(model_folder + model_file_name, 'rb'))
     processed_folder_path = 'data_processed/'
     x_sample = pd.read_csv(processed_folder_path + "sample.csv")
     predicted_y = reg_model.predict(x_sample[feature_cols])
     print(predicted_y)
+
+
+def calculate_and_write_evaluation_metrics(model_name, y_test, y_predicted, mode):
+    mean_avg_perentage_error = mean_absolute_percentage_error(y_test, y_predicted)
+    root_mean_square_error = np.sqrt(mean_squared_error(y_test, y_predicted))
+    with open(evaluation_folder + evaluation_file_name, mode) as f:
+        f.write(model_name + " metrics results:")
+        f.write(("\nMean Average Percentage Error: " + str(mean_avg_perentage_error)))
+        f.write("\nRoot Mean Square Error: " + str(root_mean_square_error))
+        f.close()
+
+
+def linear_reg_with_grid_cv(x_train, y_train, x_test, y_test):
+    linear_model = LinearRegression()
+    parameters = {'fit_intercept': [True, False], 'normalize': [True, False], 'copy_X': [True, False]}
+    grid = GridSearchCV(linear_model, parameters, cv=5)
+    grid.fit(x_train, y_train)
+    # below 2 lines are saving models into the model folder
+    filename = 'grid_cv_regression_model.sav'
+    pickle.dump(grid, open(model_folder + filename, 'wb'))
+    #  Uncomment below to load directly from model folder and comment above ones
+    # grid = pickle.load(open(model_folder + filename, 'rb'))
+    y_pred = grid.predict(x_test)
+    calculate_and_write_evaluation_metrics("\n\nGrid Search CV with Linear Regression", y_test, y_pred, "a")
+
+
+def lasso_model_and_predict(x_train, y_train, x_test, y_test):
+    lasso_model = Lasso(alpha=0.001, random_state=10, max_iter=10000, normalize=True)
+    lasso_model.fit(x_train, y_train)
+    # below 2 lines are saving models into the model folder
+    filename = 'lasso_regression_model.sav'
+    pickle.dump(lasso_model, open(model_folder + filename, 'wb'))
+    #  Uncomment below to load directly from model folder and comment above ones
+    # lasso_model = pickle.load(open(model_folder + filename, 'rb'))
+    y_pred = lasso_model.predict(x_test)
+    calculate_and_write_evaluation_metrics("\n\nLasso Regression", y_test, y_pred, "a")
+
+
+def ridge_model_and_predict(x_train, y_train, x_test, y_test):
+    ridge_reg_model = Ridge(alpha=0.001, random_state=10, max_iter=10000, normalize=True)
+    parameters = {'fit_intercept': [True, False], 'normalize': [True, False], 'copy_X': [True, False]}
+    grid_ridge_reg_model = GridSearchCV(ridge_reg_model, parameters, cv=2)
+    grid_ridge_reg_model.fit(x_train, y_train)
+    # below 2 lines are saving models into the model folder
+    filename = 'ridge_regression_model.sav'
+    pickle.dump(grid_ridge_reg_model, open(model_folder + filename, 'wb'))
+    #  Uncomment below to load directly from model folder and comment above ones
+    # grid_ridge_reg_model = pickle.load(open(model_folder + filename, 'rb'))
+    y_pred = grid_ridge_reg_model.predict(x_test)
+    calculate_and_write_evaluation_metrics("\n\nRidge Regression", y_test, y_pred, "a")
 
 
 def standardize(df):
--- wf_ml_prediction.py
+++ wf_ml_prediction.py
@@ -3,11 +3,9 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import Ridge
-from sklearn.linear_model import Lasso
-from sklearn.model_selection import GridSearchCV
-from sklearn.linear_model import LinearRegression
+
 import warnings
+import wf_ml_evaluation
 
 warnings.filterwarnings("ignore")
 evaluation_folder = "evaluation/"
@@ -15,67 +13,11 @@ evaluation_file_name = "summary.txt"
 model_folder = "models/"
 
 
-def linear_reg_with_grid_cv(x_train, y_train, x_test, y_test):
-    linear_model = LinearRegression()
-    parameters = {'fit_intercept': [True, False], 'normalize': [True, False], 'copy_X': [True, False]}
-    grid = GridSearchCV(linear_model, parameters, cv=5)
-    grid.fit(x_train, y_train)
-    # below 2 lines are saving models into the model folder
-    filename = 'grid_cv_regression_model.sav'
-    pickle.dump(grid, open(model_folder + filename, 'wb'))
-    #  Uncomment below to load directly from model folder and comment above ones
-    # grid = pickle.load(open(model_folder + filename, 'rb'))
-    y_pred = grid.predict(x_test)
-    with open(evaluation_folder + evaluation_file_name, "a") as f:
-        f.write("\n\nGrid Search CV with Linear Regression metrics results:")
-        f.write(("\nMean Average Percentage Error: " + str(mean_absolute_percentage_error(y_test, y_pred))))
-        f.write("\nRoot Mean Square Error:, " + str(np.sqrt(mean_squared_error(y_test, y_pred))))
-        f.close()
-
-
-def lasso_model_and_predict(x_train, y_train, x_test, y_test):
-    lasso_model = Lasso(alpha=0.001, random_state=10, max_iter=10000, normalize=True)
-    lasso_model.fit(x_train, y_train)
-    # below 2 lines are saving models into the model folder
-    filename = 'lasso_regression_model.sav'
-    pickle.dump(lasso_model, open(model_folder + filename, 'wb'))
-    #  Uncomment below to load directly from model folder and comment above ones
-    # lasso_model = pickle.load(open(model_folder + filename, 'rb'))
-    y_pred = lasso_model.predict(x_test)
-    with open(evaluation_folder + evaluation_file_name, "a") as f:
-        f.write("\n\nLasso Regression metrics results:")
-        f.write(("\nMean Average Percentage Error: " + str(mean_absolute_percentage_error(y_test, y_pred))))
-        f.write("\nRoot Mean Square Error:, " + str(np.sqrt(mean_squared_error(y_test, y_pred))))
-        f.close()
-
-
-def ridge_model_and_predict(x_train, y_train, x_test, y_test):
-    ridge_reg_model = Ridge(alpha=0.001, random_state=10, max_iter=10000, normalize=True)
-    parameters = {'fit_intercept': [True, False], 'normalize': [True, False], 'copy_X': [True, False]}
-    grid_ridge_reg_model = GridSearchCV(ridge_reg_model, parameters, cv=2)
-    grid_ridge_reg_model.fit(x_train, y_train)
-    # below 2 lines are saving models into the model folder
-    filename = 'ridge_regression_model.sav'
-    pickle.dump(grid_ridge_reg_model, open(model_folder + filename, 'wb'))
-    #  Uncomment below to load directly from model folder and comment above ones
-    # grid_ridge_reg_model = pickle.load(open(model_folder + filename, 'rb'))
-    y_pred = grid_ridge_reg_model.predict(x_test)
-    with open(evaluation_folder + evaluation_file_name, "a") as f:
-        f.write("\n\nRidge Regression metrics results:")
-        f.write(("\nMean Average Percentage Error: " + str(mean_absolute_percentage_error(y_test, y_pred))))
-        f.write("\nRoot Mean Square Error:, " + str(np.sqrt(mean_squared_error(y_test, y_pred))))
-        f.close()
-
-
 def load_reg_model_and_predict(x_test, y_test):
     model_file_name = "linear_regression_model.sav"
     grid_reg_model = pickle.load(open(model_folder + model_file_name, 'rb'))
     predicted_y = grid_reg_model.predict(x_test)
-    with open(evaluation_folder + evaluation_file_name, "w") as f:
-        f.write("Simple Linear Regression metrics results:")
-        f.write(("\nMean Average Percentage Error: " + str(mean_absolute_percentage_error(y_test, predicted_y))))
-        f.write("\nRoot Mean Square Error:, " + str(np.sqrt(mean_squared_error(y_test, predicted_y))))
-        f.close()
+    wf_ml_evaluation.calculate_and_write_evaluation_metrics("Basic Linear Regression", y_test, predicted_y, "w")
 
 
 def main():
@@ -93,12 +35,11 @@ def main():
     y_train = y_train['overall']
     y_test = y_test['overall']
     load_reg_model_and_predict(x_test, y_test)
-    linear_reg_with_grid_cv(x_train, y_train, x_test, y_test)
-    ridge_model_and_predict(x_train, y_train, x_test, y_test)
-    lasso_model_and_predict(x_train, y_train, x_test, y_test)
+    wf_ml_evaluation.linear_reg_with_grid_cv(x_train, y_train, x_test, y_test)
+    wf_ml_evaluation.ridge_model_and_predict(x_train, y_train, x_test, y_test)
+    wf_ml_evaluation.lasso_model_and_predict(x_train, y_train, x_test, y_test)
 
 
 if __name__ == '__main__':
     main()
-
 
